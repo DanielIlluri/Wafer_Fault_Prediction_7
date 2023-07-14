998aec32dc414662f2cc2c0bd05f98fb44f540d9
Daniel Illuri <danielilluri@gmail.com>
2023-07-14 11:42:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-prediction",
+    version="0.0.3",
+    description="ML project",
+    author="Daniel Illuri", 
+    packages=find_packages(),
+    license="MIT"
+)
